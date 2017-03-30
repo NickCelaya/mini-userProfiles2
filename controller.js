c4240918341c0da534b74d1b0d963a312b8fb827
Nick Celaya <NickCelaya07@gmail.com>
2017-03-29 23:18:31 -0600

    Completed project

--- controller.js
+++ controller.js
@@ -1,10 +1,8 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-
-
     $scope.getUsers = function() {
-        $scope.users = mainService.getUsers().then(function(param) {
-            $scope.messages = param;
+        mainService.getUsers().then(function(response) {
+            $scope.users = response.data.data;
         })
     }
 
--- service.js
+++ service.js
@@ -1,11 +1,17 @@
 angular.module('userProfiles').service('mainService', function($http) {
-    // $http request is passed into our service function to retrieve data from server with api.
 
-    this.getUsers = function(){  //this refers to service then created a function to  return our http request
-        return $http({  // making an $http request to api on server
-					method: "GET", // using method "GET" to retrieve the api data
-					url: "https://reqres.in/api/users?page=1" //url to server api
-				})
-    };
+    this.getUsers = function() {
+        return $http({
+            method: "GET",
+            url: "https://reqres.in/api/users?page=1"
+        });
+    }
 
-}); // end of the service file
+
+
+
+
+
+
+
+});
